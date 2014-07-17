24d7b942c6421925601862a62e108283d898d4b0
Max Julian <maxjulian@gmail.com>
2014-07-17 00:49:18 -0400

    Explosion proof of concept

---
+++ Galaxy-Attackers/Assets/Scripts/SwapPrefab.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwapPrefab : MonoBehaviour {
+
+    public GameObject swapTo;
+    public KeyCode swapOnKey;
+    public float explosionRadius = 5.0f;
+    public float explosionPower = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKey(swapOnKey))
+        {
+            GameObject swap = (GameObject) Instantiate(swapTo, this.transform.position, this.transform.rotation);
+
+            foreach (Transform child in swap.transform)
+            {
+                child.rigidbody.AddExplosionForce(explosionPower, transform.position, explosionRadius);
+            }
+
+            Destroy(gameObject); // Wreck yo-self
+        }
+	}
+}
