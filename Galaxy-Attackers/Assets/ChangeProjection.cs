80f7b5a5ae61936da4463ebe7700d0c977bba1be
Max Julian <maxjulian@gmail.com>
2014-07-26 17:28:38 -0400

    Fixed projection swapping

--- Galaxy-Attackers/Assets/ChangeProjection.cs
+++ Galaxy-Attackers/Assets/ChangeProjection.cs
@@ -22,52 +22,67 @@ public class ChangeProjection : MonoBehaviour {
         aspect = (Screen.width + 0.0f) / (Screen.height + 0.0f);
 
         ortho = Matrix4x4.Ortho(-orthographicSize * aspect, orthographicSize * aspect, -orthographicSize, orthographicSize, orthographicNear, orthographicFar);
+
         isOrtho = false;
+
+        Debug.Log("Perspective:\r\n" + perspective.ToString());
+        Debug.Log("Orthogonal:\r\n" + ortho.ToString());
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	    if (Input.GetKeyDown(KeyCode.Space))
+	    if (Input.GetKeyUp(KeyCode.Space))
         {
             if (isOrtho)
             {
-                BlendProjection(ortho, perspective, duration);
+                BlendProjection(perspective, ortho, duration, true);
             }
             else
             {
-                BlendProjection(perspective, ortho, duration);
+                BlendProjection(perspective, ortho, duration, false);
             }
 
             isOrtho = !isOrtho;
         }
 	}
 
-    private Matrix4x4 MatrixLerp (Matrix4x4 from, Matrix4x4 to, float t)
+    private Matrix4x4 MatrixHeavisideInterpolation(Matrix4x4 from, Matrix4x4 to, float t)
     {
         Matrix4x4 result = new Matrix4x4();
+        
+        // Step function
+        float p = 10.0f;
+        float e_p = Mathf.Exp(p);
+        float e_pt = Mathf.Exp(p * t);
+        
+        t = (1.0f + e_p) * (e_pt - 1.0f) / ((e_p - 1.0f) * (1.0f + e_pt));
+
         for (int i = 0; i < 16; i++)
         {
-            result[i] = Mathf.lerp(from[i], to[i], t);
+            result[i] = from[i] + (to[i] - from[i]) * t;
         }
         return result;
     }
 
-    private void BlendProjection (Matrix4x4 from, Matrix4x4 to, float t)
+    private void BlendProjection (Matrix4x4 from, Matrix4x4 to, float t, bool reverse)
     {
         StopAllCoroutines();
-        StartCoroutine(BlendCoroutine(from, to, t));
+        StartCoroutine(BlendCoroutine(from, to, t, reverse));
     }
 
-    private IEnumerator BlendCoroutine(Matrix4x4 from, Matrix4x4 to, float t) {
+    private IEnumerator BlendCoroutine(Matrix4x4 from, Matrix4x4 to, float t, bool reverse) {
         float startTime = Time.time;
+        float dt = 0;
 
         while (Time.time - startTime < t)
         {
-            camera.projectionMatrix = MatrixLerp(from, to, (Time.time - startTime) / t);
-            Debug.Log(camera.projectionMatrix.m23);
+            dt = (Time.time - startTime) / t;
+            dt = (reverse) ? 1.0f - dt : dt;
+
+            camera.projectionMatrix = MatrixHeavisideInterpolation(from, to, dt);
             yield return null;
         }
-        Debug.Log(startTime + " -> " + Time.time);
-        camera.projectionMatrix = to;
+
+        camera.projectionMatrix = reverse ? from : to;
     }
 }
